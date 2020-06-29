9b8dfa00e1c8c29d982484e2c1a131b37b203a38
Dinesh Panda <dineshpanda92@gmail.com>
2020-06-29 14:05:34 +0530

    Indirect association between DirectorResource and ActorResource

--- app/resources/director_resource.rb
+++ app/resources/director_resource.rb
@@ -16,4 +16,18 @@ class DirectorResource < ApplicationResource
 
   # Indirect associations
 
+  has_many :casts, resource: ActorResource do
+    assign_each do |director, actors|
+      actors.select do |a|
+        a.id.in?(director.casts.map(&:id))
+      end
+    end
+  end
+
+
+  filter :actor_id, :integer do
+    eq do |scope, value|
+      scope.eager_load(:casts).where(:roles => {:actor_id => value})
+    end
+  end
 end
