4f031c207732a3fdb8ddb40a986b886474365cb2
Dinesh Panda <dineshpanda92@gmail.com>
2020-06-29 14:05:31 +0530

    Generate graphiti resources for actors

---
+++ app/resources/actor_resource.rb
@@ -0,0 +1,16 @@
+class ActorResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :name, :string
+  attribute :dob, :string
+  attribute :bio, :string
+  attribute :image, :string
+  attribute :email, :string
+  attribute :password, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
