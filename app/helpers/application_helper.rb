e941dd7d0fcee592350b9c599a8ca1499aded7e7
Dinesh Panda <dineshpanda92@gmail.com>
2020-06-29 15:02:50 +0530

    3rd draft

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,17 @@ module ApplicationHelper
     end
   end
 
+def link_to_show_or_back(
+      object,
+      show_content = "Show",
+      back_content = "Back",
+      options = {}
+    )
+  if request.path == url_for(object)
+    link_to back_content, :back, options
+  else
+    link_to show_content, object, options
+  end
+end
+
 end
