635e275f22280f54db2a2a40563b9dec5423d3f7
Dinesh Panda <dineshpanda92@gmail.com>
2020-07-02 00:21:06 +0530

    29th draft

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -13,7 +13,7 @@ class Api::V1::MoviesController < Api::V1::GraphitiController
     movie = MovieResource.build(params)
 
     if movie.save
-      render jsonapi: movie, status: 201
+      render jsonapi: movie, status: :created
     else
       render jsonapi_errors: movie
     end
@@ -22,7 +22,7 @@ class Api::V1::MoviesController < Api::V1::GraphitiController
   def update
     movie = MovieResource.find(params)
 
-    if movie.update_attributes
+    if movie.update
       render jsonapi: movie
     else
       render jsonapi_errors: movie
@@ -33,7 +33,7 @@ class Api::V1::MoviesController < Api::V1::GraphitiController
     movie = MovieResource.find(params)
 
     if movie.destroy
-      render jsonapi: { meta: {} }, status: 200
+      render jsonapi: { meta: {} }, status: :ok
     else
       render jsonapi_errors: movie
     end
